35cf125d57fac85430621e2b7cfc944da88173af
rpenn <ryanpenn00@gmail.com>
2016-03-12 13:49:40 -0500

    added angular to app

---
+++ public/app.js
@@ -0,0 +1,13 @@
+var app = angular.module('FlashCards', []);
+
+app.controller('FlashcardCtrl', function($scope) {
+	$scope.flashCard = {
+    question: 'What is Angular?',
+    answers: [
+      { text: 'A front-end framework for great power!', correct: true },
+      { text: 'Something lame, who cares, whatever.', correct: false },
+      { text: 'Some kind of fish, right?', correct: false }
+    ]
+	};
+});
+
